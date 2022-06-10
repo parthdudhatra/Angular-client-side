5806c25c43a85e0ddc3a897df5caaa73639ed61b
parth <parth.dudhatra@aspiresoftserv.in>
2022-06-10 14:26:17 +0530

    changes

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,6 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormBuilder , Validators} from '@angular/forms'
+import { FormGroup, FormBuilder, Validators } from '@angular/forms'
 import { Router } from '@angular/router';
 import { ApiService } from '../shraed/api.service';
 
@@ -14,44 +14,32 @@ export class LoginComponent implements OnInit {
 
   public loginForm !: FormGroup
   constructor(
-    private formBuilder : FormBuilder,
-     private http : HttpClient,
-     private router : Router,
-     private api : ApiService ) { }
+    private formBuilder: FormBuilder,
+    private http: HttpClient,
+    private router: Router,
+    private api: ApiService) { }
 
   ngOnInit(): void {
     this.loginForm = this.formBuilder.group({
-      email : ['', Validators.required],
-      password : ['', Validators.required]
+      email: ['', Validators.required],
+      password: ['', Validators.required]
     })
   }
-  login(){
-    const data = this.loginForm.value
-    this.api.login(data)
-      .subscribe((res) => {
-        if(res.Success){
+
+  login() {
+    if (this.loginForm.valid) {
+      this.api.login(this.loginForm.value).subscribe(
+        res => {
+          console.log(res);
           alert(res.message)
+          localStorage.setItem("token", res.token);
           this.loginForm.reset();
-        this.router.navigate(['employee-list'])
-        }else {
-          alert(res.message)
-        }
-      })
-
-    // this.http.get<any>("http://localhost:3000/signupUsers")
-    // .subscribe((res) => {
-    //   const user = res.find((a : any) => {
-    //     return a.email === this.loginForm.value.email && a.password === this.loginForm.value.password
-    //   })
-    //   if(user){
-    //     alert("Login Success");
-    //     this.loginForm.reset();
-    //     this.router.navigate(['dashborad'])
-    //   }
-    //   else{
-    //     alert("User Not Found")
-    //   }
-    // })
-  }
-
+          this.router.navigate(["/employee-list"]);
+        },
+        err => {
+          alert('Login not successfully')
+          console.log(err);
+        });
+      }
+    }
 }
